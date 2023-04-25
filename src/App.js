eab52a44a123101068772be0db55b36934378d61
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-25 20:18:19 +0100

    Start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+
+const adverts = [
+    {
+        content: 
+        "Se vende horno",
+        userId: 1,
+        id: 1,
+    },{
+        content: 
+        "Se busca horno",
+        userId: 2,
+        id: 2,
+    }
+];
+
+function App () {
+return (<div className='App'>
+    <ul>
+        {
+            adverts.map(advert => (<li key={advert.id}>{advert.content}</li>
+            ))}
+    </ul>
+</div>
+)};
 
 export default App;
+
