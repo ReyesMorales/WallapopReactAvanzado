eb3e0b6925b4336bb71aaa9d2d54012a75661eca
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-27 17:30:28 +0100

    Add state and handle change

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,28 +1,57 @@
+import { useState } from "react";
 import Button from "../shared/Button";
 import { login } from "./service"
 
 
-function LoginPage() {
+function LoginPage({ onLogin }) {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+    });
+    
     const handleSubmit = async event => {
         event.preventDefault();
-
-        const response = await login({
-            username: event.target.username.value,
-            password: event.target.password.value
-        });
-
-        console.log(response);
-
+        await login(credentials);
+        
+        onLogin();
     };
 
+    const handleChange = event => {
+        
+        setCredentials({...credentials,[event.target.name]: event.target.value});
+    }
+
+    console.log(credentials.username, credentials.password);
 
     return (
     <div>
         <h1>Log in to Nodepop</h1>
         <form onSubmit={handleSubmit}>
-            <input type="text" name="username" />
-            <input type="password" name="password" />
-            <Button type="submit" variant="secondary">Log in</Button>
+            <input 
+            type="text" 
+            name="username" 
+            placeholder="Email"
+            onChange={handleChange} 
+            autoComplete="false"
+            value={credentials.username}
+            />
+            <input 
+            type="password" 
+            name="password" 
+            placeholder="Password"
+            onChange={handleChange} 
+            autoComplete="false"
+            value={credentials.password}
+            />
+            <Button 
+            type="submit" 
+            variant="secondary">
+                Log in
+            </Button>
+            <br />
+            <label>
+            <input type="checkbox" name="password-checkbox" defaultChecked={true} /> Recordar contraseña 
+            </label>
         </form>
     </div>
     );
