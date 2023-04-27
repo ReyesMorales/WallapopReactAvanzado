9ae31b788df6efda39a2cf71b56af1abb1b2846c
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-27 17:29:47 +0100

    Add authorization token

--- src/api/client.js
+++ src/api/client.js
@@ -6,4 +6,6 @@ const client = axios.create({
 
 client.interceptors.response.use((response) => response.data);
 
+export const setAuthorizationHeader = token => (client.defaults.headers.common["Authorization"] = `Bearer ${token}`);
+
 export default client;
