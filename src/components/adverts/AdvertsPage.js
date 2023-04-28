8c2a2b51ed9de022e94cb30b14fec824e0ee2b11
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-28 23:38:51 +0100

    Add Advert component

--- src/components/adverts/AdvertsPage.js
+++ src/components/adverts/AdvertsPage.js
@@ -2,34 +2,34 @@ import "./styles.css";
 import { useEffect, useState } from "react";
 import { getLatestAdverts } from "./service";
 import Button from '../shared/Button'
-import { logout } from "../auth/service";
 import Layout from '../layout/Layout'
+import Advert from "./Advert";
 
 
-const AdvertsPage = ({ onLogout }) => {
+const AdvertsPage = props => {
   const [adverts, setAdverts] = useState([]);
 
   useEffect(() => {
     getLatestAdverts().then((adverts) => setAdverts(adverts));
   }, []);
 
-  const handleClick = async () => {
-    await logout();
-    onLogout();
-  };
 
   return (
-    <Layout title="How are you?">
+    <>
+    <h1 className="header-title">Nodepop</h1>
+    <Layout title="Second life for your things " {...props}>
     <div className="advertsPage">
-        { /* <Button onClick={ handleClick }>Logout</Button> */}
-      {!!adverts.length ? <ul>
+       {!!adverts.length ? <ul>
         {adverts.map((advert) => (
-          <li key={advert.id}>{advert.descripcion}</li>
+        <Advert name={advert.name} sale={advert.sale} price={advert.price} tags={advert.tags}/>
         ))}
       </ul> : <Button variant="secondary">Do something</Button>}
+      <div>Adverts Page</div>
     </div>
     </Layout>
+    </>
   );
 };
 
 export default AdvertsPage;
+
