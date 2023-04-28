94076e737fc4e06628421bdb67fd32a27ed70602
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-28 11:11:45 +0100

    Add Layout

--- src/components/adverts/AdvertsPage.js
+++ src/components/adverts/AdvertsPage.js
@@ -1,22 +1,34 @@
 import "./styles.css";
 import { useEffect, useState } from "react";
 import { getLatestAdverts } from "./service";
+import Button from '../shared/Button'
+import { logout } from "../auth/service";
+import Layout from '../layout/Layout'
 
-const AdvertsPage = () => {
+
+const AdvertsPage = ({ onLogout }) => {
   const [adverts, setAdverts] = useState([]);
 
   useEffect(() => {
     getLatestAdverts().then((adverts) => setAdverts(adverts));
   }, []);
 
+  const handleClick = async () => {
+    await logout();
+    onLogout();
+  };
+
   return (
+    <Layout title="How are you?">
     <div className="advertsPage">
-      <ul>
+        { /* <Button onClick={ handleClick }>Logout</Button> */}
+      {!!adverts.length ? <ul>
         {adverts.map((advert) => (
           <li key={advert.id}>{advert.descripcion}</li>
         ))}
-      </ul>
+      </ul> : <Button variant="secondary">Do something</Button>}
     </div>
+    </Layout>
   );
 };
 
