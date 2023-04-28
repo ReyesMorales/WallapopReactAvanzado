0adbf2a1580ea6d61951ed0a25dc6c912f728eea
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-28 11:09:32 +0100

    Handle Login and Logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import "./App.css";
-import React from "react";
-/* import AdvertsPage from "./components/adverts/AdvertsPage";*/
+import React, { useState } from "react";
+import AdvertsPage from "./components/adverts/AdvertsPage";
 import Button from "./components/shared/Button";
 import LoginPage from "./components/auth/LoginPage";
+import NewAdvertPage from './components/adverts/NewAdvertPage'
 
-function App() {
+function App({ isInitiallyLogged }) {
+  const [isLogged, setIsLogged] = useState(isInitiallyLogged);
+
+  const handleLogin = () => {
+    setIsLogged(true);
+  };
+
+  const handleLogout = () => {
+    setIsLogged(false);
+  };
+
   return (
     <div className="App">
-      {/* <AdvertsPage /> */}
-      <LoginPage />
+      {isLogged ? (
+      <>
+      <AdvertsPage onLogout={handleLogout}/>
+      <NewAdvertPage />
+      </>
+      ) :(
+      <LoginPage onLogin={handleLogin}/>
+      )}
       { /* <Button variant="primary" onClick={(event) => console.log(event)}>
         Click
       </Button>
