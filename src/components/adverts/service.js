55a0f6d6fbe155bd4a14e89e759b074e88fbad6b
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-28 23:40:20 +0100

    Change URL to use Nest backend

--- src/components/adverts/service.js
+++ src/components/adverts/service.js
@@ -1,6 +1,6 @@
 import client from "../../api/client";
 
-const advertsURL = "/api/adverts";
+const advertsURL = "/api/v1/adverts";
 
 export const getLatestAdverts = () => {
   return client.get(advertsURL);
