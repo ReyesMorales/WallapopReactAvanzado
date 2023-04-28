e63a217f2e4899cd9d7ffe297bfd0d68a10208e0
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-28 11:12:14 +0100

    Create NewAdvertPage component

---
+++ src/components/adverts/NewAdvertPage.js
@@ -0,0 +1,11 @@
+import Layout from "../layout/Layout";
+
+const NewAdvertPage = () => {
+    return (
+    <Layout title="What do you need?">
+        <div>New Advert Page</div>
+    </Layout>
+    )};
+    
+
+export default NewAdvertPage;
