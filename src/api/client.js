f4cce90220abb22d83b659e3f617224440c32249
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-28 11:10:36 +0100

    Remove the authorization when logout

--- src/api/client.js
+++ src/api/client.js
@@ -8,4 +8,8 @@ client.interceptors.response.use((response) => response.data);
 
 export const setAuthorizationHeader = token => (client.defaults.headers.common["Authorization"] = `Bearer ${token}`);
 
+export const removeAuthorizationHeader = () => {
+    delete client.defaults.headers.common["Authorization"]
+}
+
 export default client;
