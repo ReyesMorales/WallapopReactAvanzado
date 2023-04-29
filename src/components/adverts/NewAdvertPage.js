15866d89bd947c1bcb38c3ca2ec175e82e86e018
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-29 20:06:55 +0100

    Add tags

--- src/components/adverts/NewAdvertPage.js
+++ src/components/adverts/NewAdvertPage.js
@@ -7,12 +7,13 @@ const NewAdvertPage = (props) => {
     name: "",
     price: "",
     sale: "",
+    tags: [],
   });
 
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
   useEffect(() => {
-    if (formData.name !== "" && formData.sale !== ""  && formData.price !== "") {
+    if (formData.name !== "" && formData.sale !== "" && formData.price !== "") {
       setButtonDisabled(false);
     } else {
       setButtonDisabled(true);
@@ -25,49 +26,108 @@ const NewAdvertPage = (props) => {
   };
 
   const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
+    const { name, value, type } = event.target;
+    if (type === "checkbox") {
+      const { checked } = event.target;
+      const selectedTags = [...formData.tags];
+      if (checked) {
+        selectedTags.push(value);
+      } else {
+        const index = selectedTags.indexOf(value);
+        if (index !== -1) {
+          selectedTags.splice(index, 1);
+        }
+      }
 
-  return (
-    <Layout title="What do you need?" {...props}>
-      <h1>New Advert Page</h1>
-      <form className="new-advert-form">
-        <label>
-          Nombre:
-          <input type="text" name="name" onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Tipo:
-          <select type="select-one" name="sale" onChange={handleChange}>
-            <option value="">-- Selecciona una opción --</option>
-            <option name="venta" value={true}>
-              Venta
-            </option>
-            <option name="compra" value={false}>
-              Compra
-            </option>
-          </select>
-          <br></br>
-        </label>
-        <label>
-          Precio:
-          <input type="text" name="price" onChange={handleChange} />
-        </label>
-      </form>
-      <Button
-        type="submit"
-        name="button"
-        onClick={handleSubmit}
-        variant="secondary"
-        disabled={buttonDisabled}
-      >Crear
-      </Button>
-    </Layout>
-  );
-};
+      setFormData({
+        ...formData,
+        [name]: selectedTags,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+}
+
+    return (
+      <Layout title="What do you need?" {...props}>
+        <h1>New Advert Page</h1>
+        <form className="new-advert-form">
+          <label>
+            Nombre:
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+          <br />
+          <label>
+            Tipo:
+            <select type="select-one" name="sale" onChange={handleChange}>
+              <option value="">-- Selecciona una opción --</option>
+              <option name="venta" value={true}>
+                Venta
+              </option>
+              <option name="compra" value={false}>
+                Compra
+              </option>
+            </select>
+            <br />
+          </label>
+          <label>
+            Precio:
+            <input type="text" name="price" onChange={handleChange} />
+          </label>
+          <br />
+          <label>
+            Tags:
+            <input
+              type="checkbox"
+              name="tags"
+              value="lifestyle"
+              checked={formData.tags.includes("lifestyle")}
+              onChange={handleChange}
+            />
+            <label>Lifestyle</label>
+            <br />
+            <input
+              type="checkbox"
+              name="tags"
+              value="mobile"
+              checked={formData.tags.includes("mobile")}
+              onChange={handleChange}
+            />
+            <label>Mobile</label>
+            <br />
+            <input
+              type="checkbox"
+              name="tags"
+              value="motor"
+              checked={formData.tags.includes("motor")}
+              onChange={handleChange}
+            />
+            <label>Motor</label>
+            <br />
+            <input
+              type="checkbox"
+              name="tags"
+              value="work"
+              checked={formData.tags.includes("work")}
+              onChange={handleChange}
+            />
+            <label>Work</label>
+          </label>
+        </form>
+        <Button
+          type="submit"
+          name="button"
+          onClick={handleSubmit}
+          variant="secondary"
+          disabled={buttonDisabled}
+        >
+          Crear
+        </Button>
+      </Layout>
+    );
+  };
 
 export default NewAdvertPage;
