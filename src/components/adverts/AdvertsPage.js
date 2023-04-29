e96ed99599f6e04084d153ddd033df83a7eafb71
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-29 19:18:43 +0100

    Change classNames

--- src/components/adverts/AdvertsPage.js
+++ src/components/adverts/AdvertsPage.js
@@ -1,35 +1,45 @@
 import "./styles.css";
 import { useEffect, useState } from "react";
 import { getLatestAdverts } from "./service";
-import Button from '../shared/Button'
-import Layout from '../layout/Layout'
+import Button from "../shared/Button";
+import Layout from "../layout/Layout";
 import Advert from "./Advert";
 
-
-const AdvertsPage = props => {
+const AdvertsPage = (props) => {
   const [adverts, setAdverts] = useState([]);
 
   useEffect(() => {
     getLatestAdverts().then((adverts) => setAdverts(adverts));
   }, []);
 
-
   return (
     <>
-    <h1 className="header-title">Nodepop</h1>
-    <Layout title="Second life for your things " {...props}>
-    <div className="advertsPage">
-       {!!adverts.length ? <ul>
-        {adverts.map((advert) => (
-        <Advert name={advert.name} sale={advert.sale} price={advert.price} tags={advert.tags}/>
-        ))}
-      </ul> : <Button variant="secondary">Do something</Button>}
-      <div>Adverts Page</div>
-    </div>
-    </Layout>
+      <div className="header">
+        <h1 className="header-title">Nodepop</h1>
+      </div>
+      <div className="main-content">
+        <Layout title="Second life for your things" {...props}>
+          <div className="adverts-container">
+            {!!adverts.length ? (
+              <ul>
+                {adverts.map((advert) => (
+                  <Advert
+                    key={advert.id}
+                    name={advert.name}
+                    sale={advert.sale}
+                    price={advert.price}
+                    tags={advert.tags}
+                  />
+                ))}
+              </ul>
+            ) : (
+              <Button className="secondary-button">Do something</Button>
+            )}
+          </div>
+        </Layout>
+      </div>
     </>
   );
 };
 
 export default AdvertsPage;
-
