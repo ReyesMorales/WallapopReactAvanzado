30fa123aa146d8c4dfe4281749db850a2e540401
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-30 22:10:28 +0100

    Remove repeat code

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,18 +1,16 @@
 import Header from './Header';
 import '../adverts/styles.css';
 
-const Layout = ({ title, children, ...rest}) => {
-    return (
-      <div>        
-        <main>
-          <h2>{title}</h2>
-          {children}
-        </main>
-        <Header {...rest} />
-      </div>
-    );
-  };
-  
-  export default Layout; 
+const Layout = ({ title, children, ...rest }) => {
+  return (
+    <div>
+      <main>
+        <h2>{title}</h2>
+        {children}
+      </main>
+      <Header {...rest} />
+    </div>
+  );
+};
 
 export default Layout;
