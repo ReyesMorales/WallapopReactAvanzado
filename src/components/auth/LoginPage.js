222511e467caf092e2881ffeb2d8c858832f51f8
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-04-30 12:50:13 +0100

    Start create remember checkbox

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,60 +1,68 @@
 import { useState } from "react";
 import Button from "../shared/Button";
-import { login } from "./service"
-
+import { login } from "./service";
 
 function LoginPage({ onLogin }) {
-    const [credentials, setCredentials] = useState({
-        email: '',
-        password: '',
-    });
-    
-    const handleSubmit = async event => {
-        event.preventDefault();
-        await login(credentials);
-        
-        onLogin();
-    };
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleChange = event => {
-        
-        setCredentials({...credentials,[event.target.name]: event.target.value});
-    }
+  const [remember, setRemember] = useState(false);
 
-    console.log(credentials.email, credentials.password);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await login(credentials);
 
-    return (
+    onLogin();
+  };
+
+  const handleChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
+
+  const handleRememberChange = (event) => {
+    setRemember(event.target.checked);
+  };
+
+  console.log(credentials.email, credentials.password);
+
+  return (
     <div>
-        <h1>Log in to Nodepop</h1>
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="text" 
-            name="email" 
-            placeholder="Email"
-            onChange={handleChange} 
-            autoComplete="false"
-            value={credentials.email}
-            />
-            <input 
-            type="password" 
-            name="password" 
-            placeholder="Password"
-            onChange={handleChange} 
-            autoComplete="false"
-            value={credentials.password}
-            />
-            <Button 
-            type="submit" 
-            variant="secondary">
-                Log in
-            </Button>
-            <br />
-            <label>
-            <input type="checkbox" name="password-checkbox" defaultChecked={true} /> Recordar contraseña 
-            </label>
-        </form>
+      <h1>Log in to Nodepop</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          autoComplete="false"
+          value={credentials.email}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          autoComplete="false"
+          value={credentials.password}
+        />
+        <br />
+        <label>
+          Recordar contraseña
+          <input
+            type="checkbox"
+            name="password-checkbox"
+            checked={remember}
+            onChange={handleRememberChange}
+          />
+        </label>
+        <Button type="submit" variant="secondary">
+          Log in
+        </Button>
+      </form>
     </div>
-    );
+  );
 }
 
 export default LoginPage;
