c1e60350eea8aac99a4564230457d6f7d87683a2
Reyes Morales <reyesmoralesdominguez@gmail.com>
2023-04-30 20:39:46 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import "./App.css";
-import React, { Fragment, useState } from "react";
-import AdvertsPage from "./components/adverts/AdvertsPage";
-import LoginPage from "./components/auth/LoginPage";
-import NewAdvertPage from './components/adverts/NewAdvertPage'
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AdvertsPage from './components/adverts/AdvertsPage';
+import LoginPage from './components/auth/LoginPage';
+import NewAdvertPage from './components/adverts/NewAdvertPage';
+import { NotFoundPage } from './components/adverts/NotFound';
+import { useIsLogged } from './hooks';
+import { DetailedAdvert } from './components/adverts/DetailedAdvert';
 
 function App({ isInitiallyLogged }) {
   const [isLogged, setIsLogged] = useState(isInitiallyLogged);
+  useIsLogged(isLogged);
 
   const handleLogin = () => {
     setIsLogged(true);
@@ -16,16 +21,29 @@ function App({ isInitiallyLogged }) {
   };
 
   return (
-    <div className="App">
-      {isLogged ? (
-      <>
-      <AdvertsPage onLogout={handleLogout} isLogged={isLogged}/>
-      <NewAdvertPage onLogout={handleLogout} isLogged={isLogged}/>
-      </>
-      ) :(
-      <LoginPage onLogin={handleLogin}/>
-      )}
-    </div>
+    <Routes>
+      <Route
+        path="/login"
+        element={<LoginPage onLogin={handleLogin} />}
+      />
+      <Route
+        path="/adverts/new"
+        element={<NewAdvertPage onLogout={handleLogout} isLogged={isLogged} />}
+      />
+      <Route
+        path="/adverts"
+        element={<AdvertsPage onLogout={handleLogout} isLogged={isLogged} />}
+      />
+      <Route
+        path="/"
+        element={<AdvertsPage onLogout={handleLogout} isLogged={isLogged} />}
+      />
+      <Route
+        path="/adverts/:id"
+        element={<DetailedAdvert onLogout={handleLogout} isLogged={isLogged} />}
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
