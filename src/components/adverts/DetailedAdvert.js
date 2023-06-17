bf6c8414f6199a50eec25734599319ed161656d3
Johann Moreno Noda <johann.moreno@intermodaltelematics.com>
2023-06-17 21:15:56 +0100

    add back button

--- src/components/adverts/DetailedAdvert.js
+++ src/components/adverts/DetailedAdvert.js
@@ -3,10 +3,15 @@ import Advert from './Advert';
 import { useParams } from 'react-router-dom';
 import { getAdvertDetail } from './service';
 import Layout from '../layout/Layout';
+import Button from '../shared/Button';
+import { useNavigate } from 'react-router-dom';
+
 
 export const DetailedAdvert = () => {
   const { id } = useParams();
   const [advert, setAdvert] = useState({});
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     getAdvertDetail(id)
@@ -27,6 +32,7 @@ export const DetailedAdvert = () => {
         price={advert.price}
         tags={advert.tags}
       />
+      <Button onClick={() => {navigate('/')}}>Back</Button>
     </Layout>
   );
 };
